97a545c9c005f460e1368c713fc20635eeb68b8e
Cheng Tang <cheng21tang@gmail.com>
2021-07-28 17:37:09 -0700

    martha beck text message completed initial layout of text message box

--- src/components/Textbox/index.js
+++ src/components/Textbox/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './style.css';
 
 function Textbox() {
-  return(
+  return (
     <>
-    
+      <div id="textbox">
+        <div className="textbox-header row">
+          <img id="martha" src="./assets/martha-beck-avatar.svg" alt="Martha Beck avatar"></img>
+          <h4>Martha Beck</h4>
+        </div>
+        <div className="textbox-body">
+          <div className="text-receive">
+            <p>the way we do anything</p>
+            <p>is the way we do everything.</p>
+          </div>
+          <div className="text-sent">
+            <p>so true!</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './home.css';
 import Sidenav from '../components/Sidenav';
+import Textbox from '../components/Textbox';
 
 function Home() {
   return (
     <>
       <Sidenav/>
+      <div id="main">
+        <Textbox></Textbox>
+      </div>
     </>
   )
 }
